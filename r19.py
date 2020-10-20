62bf05bdbbf7766706b81732414812bba167fd9a
kreativ25 <aleksandr.a.kazakevich@gmail.com>
2020-10-20 16:33:14 +0300

    2020-10-20 готовый скрипт

--- r19.py
+++ r19.py
@@ -1,7 +1,8 @@
 import pyodbc as pd
 import datetime as dt
 from config import *
-import requests as rq
+import config
+
 
 # код даты для таблицы
 date_format = dt.datetime.now().strftime('%Y%m')
@@ -21,7 +22,6 @@ for j in file_open:
 txt_max_date = max(txt_max_date)
 txt_max_date = dt.datetime.strptime(txt_max_date, '%d.%m.%Y').date()
 
-
 # формируем список заполняемых дат
 lst_date = [txt_max_date]
 
@@ -36,74 +36,41 @@ for j in range(15):
     else:
         break
 
-# print(lst_date)
+
+# формируе список действующих банков
+list_bank_prepare = cursor_support.execute(
+    "select HQ_code as BANK from bank_HQ_codes where FOR = ?",
+    True
+).fetchall()
+
+list_bank = []
+for i in list_bank_prepare:
+    if i[0]:
+        list_bank.append(i[0])
 
 file_open_r = open(filename, "r")
 
+# connection to access
+driver_a = config.driver
+con_a = pd.connect(driver_a)
+cursor_a = con_a.cursor()
 
-for j in file_open_r:
+if bd_max_date.t_s < dt.datetime.now().date() and txt_max_date == (dt.datetime.now().date() - dt.timedelta(days=1)):
+    for j in file_open_r:
 
-    BANK = j.split()[1]
-    FACT_AVG_RR = j.split()[5]
-    FACT_AVG_RR = float(FACT_AVG_RR)/1000000
-    TS = dt.datetime.now()
+        BANK = int(j.split()[1])
+        FACT_AVG_RR = j.split()[5]
+        FACT_AVG_RR = float(FACT_AVG_RR) / 1000000
+        TS = dt.datetime.now()
 
-    for i in lst_date:
-        MP_CODE = dt.datetime.strftime(i, '%Y%m')
-        MP_DATE = i
-        print(type(MP_DATE))
+        if BANK in list_bank:
+            for i in lst_date:
+                MP_CODE = int(dt.datetime.strftime(i, '%Y%m'))
 
+                MP_DATE = dt.datetime.strftime(i, '%d.%m.%Y')
+                MP_DATE = dt.datetime.strptime(MP_DATE, '%d.%m.%Y')
 
-            # cursor.execute(
-            #     'insert into FACT_AVG_RR(MP_CODE,)'
-            #)
+                cursor_a.execute('insert into FACT_AVG_RR(MP_CODE, BANK, MP_DATE, FACT_AVG_RR, TS) values(?, ?, ?, ?, ?)', MP_CODE, BANK, MP_DATE, FACT_AVG_RR, TS)
+                con_a.commit()
 
-
-
-
-
-
-
-
-# for i in lst_date:
-#     MP_DATE = i
-#     for j in file_open_r:
-#         MP_CODE = dt.datetime.strptime(j.split()[0], '%d.%m.%Y').date()
-#         MP_CODE = dt.datetime.strftime(MP_CODE, '%Y%m')
-#
-#         BANK = j.split()[1]
-#         MP_DATE = MP_DATE
-#
-#
-#     print(MP_DATE)
-
-
-
-
-
-# date_format = dt.datetime.now().strftime('%Y%m')
-
-# for q in file_open_r:
-#
-#     print(q.split()[1])
-
-
-
-
-
-
-
-# print(txt_max_date)
-# print(list_date.ddd)
-
-
-# for i in list_date:
-#     print(i)
-
-# print(list_date)
-
-
-# print(txt_max_date)
-
-
-# print(bd_max_date.t_s)
+print('Финиш')
